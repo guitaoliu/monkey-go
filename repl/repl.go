e7c18aa6fcaac0dcba33c5a43ebbd161cf6c9a68
Guitao Liu <guitao.liu@Outlook.com>
2021-08-22 14:08:30 -0400

    update: finish evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/guitaoliu/monkey-go/evaluator"
 	"github.com/guitaoliu/monkey-go/lexer"
+	"github.com/guitaoliu/monkey-go/object"
 	"github.com/guitaoliu/monkey-go/parser"
 )
 
@@ -13,6 +15,7 @@ const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Print(PROMPT)
@@ -31,8 +34,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
