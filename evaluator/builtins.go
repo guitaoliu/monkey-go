a9bc4aa9586c7207482345ff725bce1f1bfe7fc1
Guitao Liu <guitao.liu@Outlook.com>
2021-08-22 19:03:49 -0400

    update: add print support

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/guitaoliu/monkey-go/object"
+import (
+	"fmt"
+
+	"github.com/guitaoliu/monkey-go/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -96,6 +100,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
